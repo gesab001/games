31d162a96fd9197f71cfd69dbfb3dabf8518ae10
giovanni <144000giovanni@gmail.com>
2022-10-18 17:26:24 +1300

    'update'

---
+++ app.js
@@ -0,0 +1,44 @@
+
+function loadTitles(data){
+	var containerList = document.getElementById("containerList");
+	for (var x=0; x<data.length; x++){
+		var jsonItem = data[x];
+		var title =  jsonItem["title"]; 
+		var gradeLevel = jsonItem["gradeLevel"];
+		var poster = jsonItem["poster"]; 
+		var gameurl = jsonItem["gameurl"];
+
+		var itemContainer = document.createElement("DIV");
+		var gameUrlEl = document.createElement("A");
+		var titleEl = document.createElement("DIV");
+		var gradeLevelEl = document.createElement("DIV");
+		var posterEl = document.createElement("IMG");
+				posterEl.padding = "10px";
+
+		titleEl.innerHTML = title;
+		titleEl.style.wordWrap = "break-word";	
+
+		
+		gradeLevelEl.innerHTML = gradeLevel;
+		posterEl.src = poster;
+		gameUrlEl.href =  gameurl;
+		gameUrlEl.appendChild(posterEl);
+		gameUrlEl.appendChild(titleEl);
+		gameUrlEl.appendChild(gradeLevelEl);
+		itemContainer.appendChild(gameUrlEl)
+		containerList.appendChild(itemContainer);
+		
+		
+		
+
+	}
+	
+}
+
+function fetchTitles(filename){
+	
+fetch(filename)
+  .then((response) => response.json())
+  .then(loadTitles);
+  
+}
